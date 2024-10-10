fdfabcacf1f2203014ae4c98ca907f2db273f099
Farhad Rustamov <farhad.rustamov.dev@gmail.com>
2024-10-10 17:59:46 +0400

    Removing unused import

--- src/test/java/trainingpage/ModalWindow_Test.java
+++ src/test/java/trainingpage/ModalWindow_Test.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
